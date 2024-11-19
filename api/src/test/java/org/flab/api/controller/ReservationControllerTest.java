27555f8d7e447385336b7a5d986ac317fa466d48
lee chaebeen <chaebeen@mailplug.co.kr>
2024-11-19 21:21:27 +0900

    [test] ObjectMapper 사용해서 검증되지 않은 값 추가 여부 테스트하도록 수정

--- api/src/test/java/org/flab/api/controller/ReservationControllerTest.java
+++ api/src/test/java/org/flab/api/controller/ReservationControllerTest.java
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class ReservationControllerTest extends BaseIntegrationTest {
 
     @Autowired
