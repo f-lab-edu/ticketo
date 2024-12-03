fec09aba6d4767f38aef3142e78a0d030f7f9e98
lee chaebeen <dev.chaebeen@gmail.com>
2024-12-04 00:35:01 +0900

    [chore] @SuperBuilder 제거하여 필요하지 않은 설정 제거

--- api/src/main/java/org/flab/api/domain/event/dto/event/response/EventResponse.java
+++ api/src/main/java/org/flab/api/domain/event/dto/event/response/EventResponse.java
@@ -1,23 +1,13 @@
 package org.flab.api.domain.event.dto.event.response;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.flab.api.domain.event.domain.EventType;
-import org.flab.api.domain.event.dto.event.response.musical.MusicalResponse;
 
 import java.time.ZonedDateTime;
 
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME
-)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = ConcertResponse.class, name = "concert"),
-        @JsonSubTypes.Type(value = MusicalResponse.class, name = "musical")
-})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
