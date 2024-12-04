7effb72f2a9109ba59750b9135c4bd9286783ddf
lee chaebeen <dev.chaebeen@gmail.com>
2024-12-04 12:03:43 +0900

    [test] SeatListTest 수정

--- api/src/test/java/org/flab/api/domain/SeatListTest.java
+++ api/src/test/java/org/flab/api/domain/SeatListTest.java
@@ -4,15 +4,14 @@ import org.flab.api.domain.event.domain.seat.Grade;
 import org.flab.api.domain.event.domain.seat.Seat;
 import org.flab.api.domain.event.domain.seat.SeatList;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -21,24 +20,33 @@ import static org.mockito.Mockito.mock;
 @ExtendWith(MockitoExtension.class)
 public class SeatListTest {
 
-    @ParameterizedTest
-    @MethodSource("provideSeatCountTestData")
+    @Test
     @DisplayName("좌석 등급 별 좌석 수 카운트")
-    public void countSeatByGradeId(long gradeId, long expectedCount) {
+    public void countSeatByGradeId() {
         // given
-        List<Seat> seatEntityList = createSeatListMockByGradeId(gradeId, expectedCount);
+        Map<Long, Long> createSeatMap = new HashMap<>();
+        createSeatMap.put(1L, 1L);
+        createSeatMap.put(2L, 3L);
+        createSeatMap.put(3L, 4L);
+        createSeatMap.put(4L, 0L);
 
-        // when
+        List<Seat> seatEntityList = new ArrayList<>();
+        for(Map.Entry<Long, Long> entry : createSeatMap.entrySet()) {
+            seatEntityList.addAll(createSeatListMockByGradeId(entry.getKey(), entry.getValue()));
+        }
         SeatList target = new SeatList(seatEntityList);
-        long count = target.countSeatByGradeId(gradeId);
 
-        // then
-        assertThat(count).isEqualTo(expectedCount);
+        for(Map.Entry<Long, Long> entry : createSeatMap.entrySet()) {
+            // when
+            long count = target.countSeatByGradeId(entry.getKey());
+            // then
+            assertThat(count).isEqualTo(entry.getValue());
+        }
     }
 
-    private List<Seat> createSeatListMockByGradeId(long gradeId, long expectedCount) {
+    private List<Seat> createSeatListMockByGradeId(long gradeId, long createdCount) {
         List<Seat> seatList = new ArrayList<>();
-        for (int i = 0; i < expectedCount; i++) {
+        for (int i = 0; i < createdCount; i++) {
             Grade gradeMock = mock(Grade.class);
             given(gradeMock.getId()).willReturn(gradeId);
 
@@ -48,14 +56,5 @@ public class SeatListTest {
             seatList.add(seatMock);
         }
         return seatList;
-    }
-
-    private static Stream<Arguments> provideSeatCountTestData() {
-        return Stream.of(
-                Arguments.of(1, 1),
-                Arguments.of(2, 2),
-                Arguments.of(3, 4),
-                Arguments.of(7, 0)
-        );
     }
 }
