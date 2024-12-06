d86a0d8b8f8065f09f48b7208483c91c435862cf
lee chaebeen <dev.chaebeen@gmail.com>
2024-12-06 18:33:24 +0900

    [feat] preReservationPeriod 수정

--- api/src/main/java/org/flab/api/domain/event/dto/event/concert/ConcertResponse.java
+++ api/src/main/java/org/flab/api/domain/event/dto/event/concert/ConcertResponse.java
@@ -23,7 +23,7 @@ public class ConcertResponse extends EventResponse {
     public ConcertResponse(Concert concert) {
         super(concert.getId(), concert.getName(), concert.getType(), concert.getEventPeriod().getStartDateTime(), concert.getEventPeriod().getEndDateTime(),
                 concert.getRunningTime(), concert.getDescription(), concert.getReservationPeriod().getStartDateTime(), concert.getReservationPeriod().getEndDateTime(),
-                concert.getHasPreReservation(), concert.getPreReservationPeriod().getStartDateTime(), concert.getPreReservationPeriod().getEndDateTime(),
+                concert.getHasPreReservation(), concert.getNullablePeriod().getStartDateTime(), concert.getNullablePeriod().getEndDateTime(),
                 new EventCategoryResponse(concert.getCategory()), new PlaceResponse(concert.getPlace()),
                 new RegionResponse(concert.getRegion()), new EventImageResponse(concert.getImage())
         );
