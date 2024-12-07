e241a2ae50104d168c43d79ede2da367d60bb8ed
lee chaebeen <dev.chaebeen@gmail.com>
2024-12-07 16:27:13 +0900

    [remove] 미사용 코드 제거

--- api/src/main/java/org/flab/api/domain/event/repository/event/concert/ConcertRepository.java
+++
@@ -1,7 +0,0 @@
-package org.flab.api.domain.event.repository.event.concert;
-
-import org.flab.api.domain.event.domain.event.concert.Concert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConcertRepository extends JpaRepository<Concert, Long>, ConcertRepositoryCustom {
-}
